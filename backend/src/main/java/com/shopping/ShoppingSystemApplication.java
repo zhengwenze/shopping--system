c49915c96422edca70ed967a6885d84dee8cb389
zhengwenze <2017160177@qq.com>
2026-03-06 20:38:54 +0800

    添加Spring Boot主启动类

---
+++ backend/src/main/java/com/shopping/ShoppingSystemApplication.java
@@ -0,0 +1,11 @@
+package com.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingSystemApplication.class, args);
+    }
+}

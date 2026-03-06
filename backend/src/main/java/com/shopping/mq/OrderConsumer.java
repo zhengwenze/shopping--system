1007ad2ea66b6a48ba6a49ed8bcc9103f45555b2
zhengwenze <2017160177@qq.com>
2026-03-06 20:39:27 +0800

    添加订单消息消费者

---
+++ backend/src/main/java/com/shopping/mq/OrderConsumer.java
@@ -0,0 +1,22 @@
+package com.shopping.mq;
+
+import com.shopping.model.OrderMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderConsumer {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @RabbitListener(queues = MQConfig.SECKILL_ORDER_QUEUE)
+    public void handleOrder(OrderMessage msg) {
+        int updated = jdbc.update("UPDATE product SET stock = stock - 1 WHERE id = ? AND stock > 0", msg.getProductId());
+        if (updated > 0) {
+            jdbc.update("INSERT INTO order_info(user_id, product_id) VALUES(?, ?)", msg.getUserId(), msg.getProductId());
+        }
+    }
+}

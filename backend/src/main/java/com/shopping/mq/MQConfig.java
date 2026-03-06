010bae55893b07059925774e737c08c589b58ba6
zhengwenze <2017160177@qq.com>
2026-03-06 23:15:08 +0800

    修复秒杀消息的RabbitMQ序列化配置

--- backend/src/main/java/com/shopping/mq/MQConfig.java
+++ backend/src/main/java/com/shopping/mq/MQConfig.java
@@ -1,5 +1,7 @@
 package com.shopping.mq;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +13,10 @@ public class MQConfig {
     @Bean
     public Queue seckillOrderQueue() {
         return new Queue(SECKILL_ORDER_QUEUE, true);
+    }
+
+    @Bean
+    public MessageConverter rabbitMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }

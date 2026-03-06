4fb706dde71f575a3888547710a714b4fe40c2a1
zhengwenze <2017160177@qq.com>
2026-03-06 21:50:28 +0800

    为订单消费者补充事务处理

--- backend/src/main/java/com/shopping/mq/OrderConsumer.java
+++ backend/src/main/java/com/shopping/mq/OrderConsumer.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class OrderConsumer {
@@ -12,6 +13,7 @@ public class OrderConsumer {
     @Autowired
     private JdbcTemplate jdbc;
 
+    @Transactional
     @RabbitListener(queues = MQConfig.SECKILL_ORDER_QUEUE)
     public void handleOrder(OrderMessage msg) {
         int updated = jdbc.update("UPDATE product SET stock = stock - 1 WHERE id = ? AND stock > 0", msg.getProductId());

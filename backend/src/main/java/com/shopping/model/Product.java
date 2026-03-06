159323d35e977de843630255fec4adb0bc37a5f2
zhengwenze <2017160177@qq.com>
2026-03-06 20:39:03 +0800

    添加商品实体类

---
+++ backend/src/main/java/com/shopping/model/Product.java
@@ -0,0 +1,14 @@
+package com.shopping.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Integer stock;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Integer getStock() { return stock; }
+    public void setStock(Integer stock) { this.stock = stock; }
+}
